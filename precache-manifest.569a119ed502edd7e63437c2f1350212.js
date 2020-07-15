dba922a14990dd4faecfd58d9049f57d53cbe2aa
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-07-15 14:49:14 +0300

    Updates

---
+++ precache-manifest.569a119ed502edd7e63437c2f1350212.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2087b30d074a6b37d273fd7ce7328c4",
+    "url": "/test-pik/index.html"
+  },
+  {
+    "revision": "9c57d6eecdbd022d6a92",
+    "url": "/test-pik/static/css/main.0a4dd804.chunk.css"
+  },
+  {
+    "revision": "0c1499a9dacbfb2c9a19",
+    "url": "/test-pik/static/js/2.daca2a83.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/test-pik/static/js/2.daca2a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c57d6eecdbd022d6a92",
+    "url": "/test-pik/static/js/main.ffcded7e.chunk.js"
+  },
+  {
+    "revision": "0df40142b7111a0d9f69",
+    "url": "/test-pik/static/js/runtime-main.900291bc.js"
+  },
+  {
+    "revision": "f6e5277883d1ddbcb111cf0b74fcad92",
+    "url": "/test-pik/static/media/proba_pro_extralight.f6e52778.woff"
+  },
+  {
+    "revision": "47fb794d09e40ddb431333751d4d6b9d",
+    "url": "/test-pik/static/media/proba_pro_light.47fb794d.woff"
+  },
+  {
+    "revision": "d5ca0cd47e29a56802cb73d9cdca59c3",
+    "url": "/test-pik/static/media/proba_pro_medium.d5ca0cd4.woff"
+  },
+  {
+    "revision": "eb3dd3a315043d3889dc6817f7596b43",
+    "url": "/test-pik/static/media/proba_pro_regular.eb3dd3a3.woff"
+  },
+  {
+    "revision": "d41f83ca853ef6f1d7b8423e4a6d0e9b",
+    "url": "/test-pik/static/media/proba_pro_semibold.d41f83ca.woff"
+  },
+  {
+    "revision": "4c1d63380256b3409a21d74b0e9d8763",
+    "url": "/test-pik/static/media/proba_pro_thin.4c1d6338.woff"
+  }
+]);
